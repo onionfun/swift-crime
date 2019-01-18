e63783bc70bedd61eaee2d787535612f110575ce
Deirdre <deirdre@Deirdres-MacBook-Air.local>
2019-01-18 10:55:11 -0700

    adding

--- Crime-frontend/Crime-frontend/ViewController.swift
+++ Crime-frontend/Crime-frontend/ViewController.swift
@@ -26,6 +26,48 @@ class ViewController: UIViewController {
     func selectPlace(){
         print("place selected")
     }
+    
+    @IBAction func startLocationService(_ sender: UIButton){
+        //has user authorized location services
+        if CLLocationManager.authorizationStatus() == .authorizedAlways ||
+            CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            activateLocationServices()
+        }else {
+            locationManager?.requestWhenInUseAuthorization()
+        }
+    }
+    private func activateLocationServices(){
+        
+    }
+    func loadPlaces(){
+        guard let entries = loadPlist() else { fatalError("Unable to load data")}
+        for property in entries {
+            guard let name = property in entries {
+                guard let name = property["Name"] as? String,
+                let latitude = property["Latitude"] as? NSNumber,
+                let longitude = property["Longitude"] as? NSNumber,
+               // let image
+                else { fatalError("Error loading data")}
+                print("name: \(name)")
+                print("lat: \(latitude)")
+                print("long: \(longitude)")
+
+            }
+        }
+        private func loadPlist() -> [[String: Any]]? {
+            guard let plistUrl = Bundle.main.url(forResource: "Places", withExtension: "plist"),
+                let plistData=try? Data(contentsOf: plistUrl) else{return nil}
+            var placedEntries: [[String: Any]]? = nil
+            do{
+                placedEntries = try PropertyListSerialization.propertyList(from: plistData, options: [], format: nil) as? [[String: Any]]
+            }catch{
+                print("error reading plist")
+            }
+            return placedEntries
+        }
+    }
+    //impletment protocol, extension to view controller
+    extension ViewController: CLLocationManagerDelegate{
 
 
 }
